8b87a4f98ebacaa2783dbb141c0665d5f6517e3f
Hadyan Ahmad <hadyandev@gmail.com>
2019-12-19 10:55:02 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <SearchBar></SearchBar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from "./components/SearchBar";
 
 export default {
-  name: 'app',
+  name: "App",
   components: {
-    HelloWorld
+    //register the imported component
+    SearchBar // SearchBar: SearchBar
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input @input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  methods: {
+    onInput: function(event) {
+      alert(event.target.value);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
