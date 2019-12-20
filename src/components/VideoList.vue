e85038073f3d562a8e7412fdcffe373d211bcdaa
Hadyan Ahmad <hadyandev@gmail.com>
2019-12-20 14:23:24 +0700

    Add VideoListItem component and registered it to VideoList component

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,12 +1,17 @@
 <template>
   <ul>
-    VideoList
+    <VideoListItem></VideoListItem>
   </ul>
 </template>
 
 <script>
+import VideoListItem from "./VideoListItem";
+
 export default {
   name: "VideoList",
+  components: {
+    VideoListItem
+  },
   //   props without validation
   //   props: ["videos"]
   //   props with validation (recommended)
