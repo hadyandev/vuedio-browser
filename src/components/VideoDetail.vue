055c0774460ef6d743594995c38bc26786482303
Hadyan Ahmad <hadyandev@gmail.com>
2019-12-27 16:37:07 +0700

    Referencing video title and description

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,5 +1,10 @@
 <template>
-  <div v-if="video">{{ video.snippet.title }}</div>
+  <div v-if="video">
+    <div class="details">
+      <h4>{{ video.snippet.title }}</h4>
+      <p>{{ video.snippet.description }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +14,11 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.details {
+  margin-top: 10px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+}
 </style>
